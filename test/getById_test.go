d1ed38c990cc8d1dfe14de7dfeabe5c257f5da3c
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-18 14:15:26 +0100

    GetById - Try to run the test

---
+++ test/getById_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"reflect"
+	"testing"
+
+	"todo-app-go.com/v1/src/models"
+)
+
+func TestGetById(t *testing.T) {
+	todos := []models.Todo{
+		{1, "FirstTodo", false},
+		{2, "SecondTodo", false},
+		{3, "ThirdTodo", false},
+	}
+
+	todoManager := &models.TodoManager{todos}
+
+	id := 1
+	got := todoManager.GetById(id)
+	want := todos[id-1]
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v but want %v", got, want)
+	}
+}
