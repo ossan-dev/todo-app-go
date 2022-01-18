ca78d00e8891d160f46184e4b5a3a706956a3c8f
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-18 17:24:15 +0100

    Delete - Try to run the test

---
+++ src/crud/delete_test.go
@@ -0,0 +1,24 @@
+package crud
+
+import (
+	"testing"
+
+	"todo-app-go.com/v1/src/models"
+	"todo-app-go.com/v1/src/utils"
+)
+
+func TestDelete(t *testing.T) {
+	todos := []models.Todo{
+		models.NewTodo(1, "FirstTodo", false),
+		models.NewTodo(2, "SecondTodo", false),
+		models.NewTodo(3, "ThirdTodo", true),
+	}
+
+	todoManager := &TodoManager{todos}
+
+	todoManager.DeleteById(1)
+
+	_, err := todoManager.GetById(1)
+
+	utils.AssertError(t, err, ErrTodoNotFound)
+}
