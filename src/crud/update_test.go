ff4bdc2849e80ad5d827c0259f30c5a43b4bf9a6
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-18 17:10:45 +0100

    Update - Try to run the test

--- src/crud/update_test.go
+++ src/crud/update_test.go
@@ -16,11 +16,20 @@ func TestUpdate(t *testing.T) {
 
 	todoManager := &TodoManager{todos}
 
-	todoUpdated := models.NewTodo(2, "Updated text", false)
-	todoManager.Update(todoUpdated)
+	t.Run("update existing todo", func(t *testing.T) {
+		todoUpdated := models.NewTodo(2, "Updated text", false)
+		todoManager.Update(todoUpdated)
 
-	got, err := todoManager.GetById(todoUpdated.Id)
+		got, err := todoManager.GetById(todoUpdated.Id)
 
-	utils.AssertNoError(t, err)
-	utils.AssertTodosEqual(t, got, &todoUpdated)
+		utils.AssertNoError(t, err)
+		utils.AssertTodosEqual(t, got, &todoUpdated)
+	})
+
+	t.Run("update not existing todo", func(t *testing.T) {
+		todoUpdated := models.NewTodo(2, "Updated text", false)
+		_, err := todoManager.Update(todoUpdated)
+
+		utils.AssertError(t, err, ErrTodoNotFound)
+	})
 }
