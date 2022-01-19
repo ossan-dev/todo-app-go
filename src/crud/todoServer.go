d753dd091fefb5387923ef5fdf70e3802469cbe2
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-19 08:54:57 +0100

    http - getall - Write enough code to make it pass

--- src/crud/todoServer.go
+++ src/crud/todoServer.go
@@ -1,5 +1,10 @@
 package crud
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func TodoServer(w http.ResponseWriter, r *http.Request) {}
+func TodoServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `[{"id": 1, "description": "FirstTodo", "isCompleted": false}]`)
+}
