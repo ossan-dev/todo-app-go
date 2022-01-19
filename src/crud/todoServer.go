bda62707aef6c51e5deb1ee1b45ac4cafe110839
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-19 09:13:11 +0100

    http -getbyid - Write enough code to make it pass

--- src/crud/todoServer.go
+++ src/crud/todoServer.go
@@ -3,8 +3,18 @@ package crud
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func TodoServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `{"id": 1, "description": "FirstTodo", "isCompleted": false}`)
+	id := strings.TrimPrefix(r.URL.Path, "/todos/")
+	if id == "1" {
+		fmt.Fprintf(w, `{"id": 1, "description": "FirstTodo", "isCompleted": false}`)
+		return
+	}
+
+	if id == "2" {
+		fmt.Fprintf(w, `{"id": 2, "description": "SecondTodo", "isCompleted": true}`)
+		return
+	}
 }
