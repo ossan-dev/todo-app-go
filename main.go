8f0c4d570e62f712b3d2132a237195374bed99dc
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-19 09:03:40 +0100

    add http handler logic to main.go

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"todo-app-go.com/v1/src/crud"
+)
+
 func main() {
+	handler := http.HandlerFunc(crud.TodoServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
