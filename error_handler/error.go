42e1d3faac8ee9e42c1fb80f1c329a095e071793
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-21 17:53:12 +0100

    refactor errors

--- error_handler/error.go
+++ error_handler/error.go
@@ -6,3 +6,15 @@ var (
 	ErrNotFound     error = errors.New("todo not found")
 	ErrTodoNotValid error = errors.New("todo not valid")
 )
+
+const (
+	BAD_REQUEST_CODE = "BAD_REQUEST"
+
+	INVALID_BODY_REQUEST_MSG = "Invalid body request"
+)
+
+type TodoAppError struct {
+	Msg     string   `json:"msg"`
+	Code    string   `json:"code"`
+	Details []string `json:"details,omitempty"`
+}
