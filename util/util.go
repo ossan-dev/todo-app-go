505bdbacdb9260a43dcb5a2984724b02166e8a5a
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-21 11:35:54 +0100

    removed unused methods in util.go

--- util/util.go
+++ util/util.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"reflect"
 	"sort"
 	"testing"
 
@@ -12,41 +11,6 @@ func SortTodoSliceById(todos []model.Todo) {
 	sort.Slice(todos, func(t1, t2 int) bool {
 		return todos[t1].Id < todos[t2].Id
 	})
-}
-
-func AssertTodosEqual(t *testing.T, got, want model.Todo) {
-	t.Helper()
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v but want %v", got, want)
-	}
-}
-
-func AssertError(t *testing.T, got, want error) {
-	t.Helper()
-	if got == nil {
-		t.Fatal("expected an error but didn't find one")
-	}
-
-	if got.Error() != want.Error() {
-		t.Errorf("got %q but want %q", got, want)
-	}
-}
-
-func AssertNoError(t *testing.T, got error) {
-	t.Helper()
-
-	if got != nil {
-		t.Errorf("got an error but didn't want one")
-	}
-}
-
-func AssertCollectionsEqual(t *testing.T, got, want []model.Todo) {
-	t.Helper()
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v but want %v", got, want)
-	}
 }
 
 func AssertResponseBody(t *testing.T, got, want string) {
