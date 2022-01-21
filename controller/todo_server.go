f3d11f0a04bbd3fa3eb8db4447696c5bfe1f4a0f
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-01-21 09:23:42 +0100

    moved todo_server outside of 'src'

--- controller/todo_server.go
+++ controller/todo_server.go
@@ -1,4 +1,4 @@
-package servers
+package controller
 
 import (
 	"encoding/json"
--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"todo-app-go.com/v1/controller"
 	"todo-app-go.com/v1/database"
-	"todo-app-go.com/v1/src/crud/servers"
 )
 
 func main() {
 	store := database.NewInMemoryTodoStore()
-	server := servers.NewTodoServer(store)
+	server := controller.NewTodoServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
